77ace77fd3b66eda3aa47e8b1b128a557cdab2b6
Yaswitha Boppana <323103210032.yaswitha@gvpcew.ac.in>
2024-12-12 20:25:06 +0530

    Hidden the guess word by implementing process_message

--- word_sketch/lib/word_sketch_web/channels/room.ex
+++ word_sketch/lib/word_sketch_web/channels/room.ex
@@ -1,13 +1,16 @@
 defmodule WordSketchWeb.RoomChannel do
   use Phoenix.Channel
+  alias WordSketch.Chat
+
 
   def join("room" <> roomCode, _payload, socket) do
     {:ok, socket}
   end
 
   def handle_in("new_message", %{"message" => message, "username" => username, "roomCode" => roomCode}, socket) do
+    modified_message = Chat.process_message(username, message, "startgame")
     broadcast!(socket, "new_message", %{
-      message: message,
+      message: modified_message,
       username: username,
       roomCode: roomCode
     })
