b3fc43751210a2b4d069ae3709b72b3328b6c489
Yaswitha Boppana <323103210032.yaswitha@gvpcew.ac.in>
2024-12-12 09:23:18 +0530

    Implemented a quick fix for the User socket connect function parameter error

--- word_sketch/lib/word_sketch_web/channels/user_socket.ex
+++ word_sketch/lib/word_sketch_web/channels/user_socket.ex
@@ -3,9 +3,17 @@ defmodule WordSketchWeb.UserSocket do
 
   channel "room", WordSketchWeb.RoomChannel
 
+  # @impl true
+  # def connect(%{"username" => username, "vsn" => _vsn}, socket, _connect_info) do
+  #   {:ok, assign(socket, :username, username)}
+  # end
+
   @impl true
-  def connect(%{"username" => username, "vsn" => _vsn}, socket, _connect_info) do
-    {:ok, assign(socket, :username, username)}
+  def connect(params, socket, _connect_info) do
+    case params do
+      %{"username" => username, "vsn" => _vsn} -> {:ok, assign(socket, :username, username)}
+      %{"vsn" => _vsn} -> {:ok, assign(socket, :username, "Anonymous")}
+    end
   end
 
   @impl true
