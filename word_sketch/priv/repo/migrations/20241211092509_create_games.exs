a86a735fb2d0e73b31ddff56225d8bd929aa91d1
Sharon Prabhu <sharongraceprabhu@gmail.com>
2024-12-13 15:16:58 +0530

    Deleted a migration file and made the necessary changes in create_games migration file

--- word_sketch/priv/repo/migrations/20241211092509_create_games.exs
+++ word_sketch/priv/repo/migrations/20241211092509_create_games.exs
@@ -15,5 +15,18 @@ defmodule WordSketch.Repo.Migrations.CreateGames do
     create index(:games, [:room_code])
     create index(:games, [:creator_name])
 
+    # Modify the table to remove NOT NULL constraints
+    drop constraint(:games, "games_creator_id_fkey")
+    drop constraint(:games, "games_word_id_fkey")
+
+    alter table(:games) do
+      modify :creator_id, references(:users, on_delete: :nothing), null: true
+      modify :word_id, references(:words, on_delete: :nothing), null: true
+      modify :room_code, :string, null: true
+    end
+
+    # Drop old index and create new unique index for room_code
+    execute("DROP INDEX IF EXISTS games_room_code_index")
+    create unique_index(:games, [:room_code])
   end
 end
