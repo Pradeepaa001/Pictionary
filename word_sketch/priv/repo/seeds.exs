c9cdcc170f805c7d9e0e6b18063fdab57d927e0c
Yaswitha Boppana <323103210032.yaswitha@gvpcew.ac.in>
2024-12-13 22:04:09 +0530

    Worked on words database, run the following commands: - mix ecto.migrate - mix run priv/repo/seeds.exs note - if the migrate is giving issues drop, create and then migrate

--- word_sketch/priv/repo/seeds.exs
+++ word_sketch/priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias WordSketch.Repo
+alias WordSketch.Words.Word
+
+File.stream!("priv/repo/myWords.txt")
+|> Stream.map(&String.trim/1)
+|> Enum.each(fn word ->
+  cleaned_word = String.replace(word, "-", " ")
+  %Word{}
+  |> Word.changeset(%{word: cleaned_word})
+  |> Repo.insert!()
+end)
